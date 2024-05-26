3abd5e10d9b2f0258f185df624f96d8e0d3bea97
heshiwei <heshiweiol@outlook.com>
2024-05-26 15:41:37 +0800

    线程池优化-----最终版

--- mysql_connector/DatabaseSession/DatabaseSession.py
+++ mysql_connector/DatabaseSession/DatabaseSession.py
@@ -1,19 +1,25 @@
 import mysql.connector
+from mysql.connector import pooling
 import logging
-from contextlib import contextmanager
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
+
 # 自定义数据库连接操作
 # 用于管理连接connection 和 close
-from mysql.connector import pooling
-
-
 class DatabaseSession:
-    def __init__(self, config):
+
+    def __init__(self, config):  # 构造方法，传入config连接配置
         self.pool = pooling.MySQLConnectionPool(pool_name="mypool",
-                                                pool_size=10,
-                                                **config)
+                                                pool_size=30,
+                                                **config)  # **self.config,自动解包config,否则需要手动取出每一个配置进行设置
+        # connection = mysql.connector.connect(
+        #     host=self.config['host'],
+        #     user=self.config['user'],
+        #     password=self.config['password'],
+        #     database=self.config['database']
+        # )
         self.connection = None
 
     def open(self):
@@ -24,20 +30,11 @@ class DatabaseSession:
             self.connection.close()
             self.connection = None
 
-    # @contextmanager
-    # def cursor(self):
-    #     cursor = None
-    #     try:
-    #         cursor = self.execute("SELECT 1")  # 这里只是为了示例
-    #         yield cursor
-    #     finally:
-    #         if cursor:
-    #             cursor.close()
-
     def execute(self, query, params=None):
         try:
             if self.connection is None:
                 self.open()
+            #     使用缓冲的游标：使用缓冲游标可以自动读取所有结果到客户端内存中，这样可以防止 Unread result found 错误
             cursor = self.connection.cursor(buffered=True, dictionary=True)
             cursor.execute(query, params or ())
             return cursor
